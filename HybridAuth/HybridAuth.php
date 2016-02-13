ca550046f3fa8445bdf870322b2b92cadacafbe6
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2016-02-13 22:01:21 +0200

    Fixed error. Bug fixes when incorrectly set the id or secret key

--- HybridAuth/HybridAuth.php
+++ HybridAuth/HybridAuth.php
@@ -12,6 +12,7 @@ namespace Scandiweb\SocialLogin\HybridAuth;
 
 use Hybrid_Auth;
 use Magento\Framework\UrlInterface;
+use Exception;
 
 class HybridAuth extends Hybrid_Auth
 {
@@ -36,54 +37,61 @@ class HybridAuth extends Hybrid_Auth
         $vendorPath = require BP . '/app/etc/vendor_path.php';
         $vendorPath = BP . "/{$vendorPath}/";
 
-        parent::__construct([
-            'base_url' => $url->getBaseUrl() . '/sociallogin/endpoint',
-            'providers' => [
-                ucfirst(self::FACEBOOK) => [
-                    'enabled' => $config->isProviderEnabled(self::FACEBOOK),
-                    'keys'    => [
-                        'id' => $config->getProviderApiKey(self::FACEBOOK),
-                        'secret' => $config->getProviderApiSecret(
-                            self::FACEBOOK
-                        )
+        try {
+            parent::__construct([
+                'base_url' => $url->getBaseUrl() . '/sociallogin/endpoint',
+                'providers' => [
+                    ucfirst(self::FACEBOOK) => [
+                        'enabled' => $config->isProviderEnabled(self::FACEBOOK),
+                        'keys'    => [
+                            'id' => $config->getProviderApiKey(self::FACEBOOK),
+                            'secret' => $config->getProviderApiSecret(
+                                self::FACEBOOK
+                            )
+                        ],
+                        'scope' => 'email'
                     ],
-                    'scope' => 'email'
-                ],
-                ucfirst(self::TWITTER) => [
-                    'enabled' => $config->isProviderEnabled(self::TWITTER),
-                    'keys'    => [
-                        'key'    => $config->getProviderApiKey(self::TWITTER),
-                        'secret' => $config->getProviderApiSecret(self::TWITTER)
+                    ucfirst(self::TWITTER) => [
+                        'enabled' => $config->isProviderEnabled(self::TWITTER),
+                        'keys'    => [
+                            'key'    => $config->getProviderApiKey(self::TWITTER),
+                            'secret' => $config->getProviderApiSecret(self::TWITTER)
+                        ],
+                        'includeEmail' => true
                     ],
-                    'includeEmail' => true
-                ],
-                ucfirst(self::GOOGLE) => [
-                    'enabled' => $config->isProviderEnabled(self::GOOGLE),
-                    'keys'    => [
-                        'id'     => $config->getProviderApiKey(self::GOOGLE),
-                        'secret' => $config->getProviderApiSecret(self::GOOGLE)
-                    ]
-                ],
-                ucfirst(self::INSTAGRAM) => [
-                    'enabled' => $config->isProviderEnabled(self::INSTAGRAM),
-                    'keys'    => [
-                        'id'     => $config->getProviderApiKey(self::INSTAGRAM),
-                        'secret' => $config->getProviderApiSecret(self::INSTAGRAM)
+                    ucfirst(self::GOOGLE) => [
+                        'enabled' => $config->isProviderEnabled(self::GOOGLE),
+                        'keys'    => [
+                            'id'     => $config->getProviderApiKey(self::GOOGLE),
+                            'secret' => $config->getProviderApiSecret(self::GOOGLE)
+                        ]
                     ],
-                    'wrapper' => [
-                        'path'  => $vendorPath . 'hybridauth/hybridauth/additional-providers/hybridauth-instagram/Providers/Instagram.php',
-                        'class' => 'Hybrid_Providers_Instagram'
-                    ]
-                ],
-                ucfirst(self::YAHOO) => [
-                    'enabled' => $config->isProviderEnabled(self::YAHOO),
-                    'keys'    => [
-                        'key'    => $config->getProviderApiKey(self::YAHOO),
-                        'secret' => $config->getProviderApiSecret(self::YAHOO)
+                    ucfirst(self::INSTAGRAM) => [
+                        'enabled' => $config->isProviderEnabled(self::INSTAGRAM),
+                        'keys'    => [
+                            'id'     => $config->getProviderApiKey(self::INSTAGRAM),
+                            'secret' => $config->getProviderApiSecret(self::INSTAGRAM)
+                        ],
+                        'wrapper' => [
+                            'path'  => $vendorPath . 'hybridauth/hybridauth/additional-providers/hybridauth-instagram/Providers/Instagram.php',
+                            'class' => 'Hybrid_Providers_Instagram'
+                        ]
+                    ],
+                    ucfirst(self::YAHOO) => [
+                        'enabled' => $config->isProviderEnabled(self::YAHOO),
+                        'keys'    => [
+                            'key'    => $config->getProviderApiKey(self::YAHOO),
+                            'secret' => $config->getProviderApiSecret(self::YAHOO)
+                        ]
                     ]
                 ]
-            ]
-        ]);
+            ]);
+        } catch (Exception $e) {
+            // Kludge, but corrects error - Undefined index: oauth_token in OAuth1Client.php
+            // Throws a exception: "Notice: Undefined index: providers"
+            // TODO: Rewrite this using a more better solution
+            parent::__construct([]);
+        }
     }
 
 }
